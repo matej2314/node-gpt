7d908454af41e3c718d85491dd259a0161033beb
matej2314 <mateo2314@gmail.com>
2024-08-31 20:53:52 +0200

    Add complete frontend files, and server file after little modifications. Now the project is done.

--- gpt_api/script.js
+++ gpt_api/script.js
@@ -1,26 +1,40 @@
 'use strict';
 
-// async function sendPostRequest() {
-// 	try {
-// 		const response = await fetch('http://127.0.0.1:3005/getchat', {
-// 			method: 'POST',
-// 			headers: {
-// 				'Content-Type': 'application/json', // Nagłówek Content-Type
-// 			},
-// 			body: JSON.stringify({
-// 				content: 'Capital of Germany', // Poprawne ciało żądania
-// 			}),
-// 		});
+const responseField = document.querySelector('.response-ai');
+const promptfield = document.querySelector('.prompt');
+const btnSend = document.querySelector('.btn-send');
+const btnClear = document.querySelector('.btn-clear');
 
-// 		if (!response.ok) {
-// 			throw new Error('Network response was not ok');
-// 		}
+btnSend.addEventListener('click', function () {
+	sendPostRequest();
+	promptfield.value = ' ';
+});
 
-// 		const responseData = await response.text(); // Odczytaj dane jako tekst
-// 		console.log(responseData); // Wypisz odpowiedź na konsolę
-// 	} catch (error) {
-// 		console.error('Error:', error);
-// 	}
-// }
+btnClear.addEventListener('click', function () {
+	document.querySelector('.response-ai').innerHTML = ' ';
+});
 
-// sendPostRequest();
+async function sendPostRequest() {
+	try {
+		let prompt = document.querySelector('.prompt').value;
+		const response = await fetch('http://127.0.0.1:3005/getchat', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json', // Nagłówek Content-Type
+			},
+			body: JSON.stringify({
+				content: prompt, // Poprawne ciało żądania
+			}),
+		});
+
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+
+		const responseData = await response.text(); // Odczytaj dane jako tekst
+		console.log(responseData); // Wypisz odpowiedź na konsolę
+		responseField.textContent = responseData;
+	} catch (error) {
+		console.error('Error:', error);
+	}
+}
